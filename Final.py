3dac060b1cb54520b6e535fdc4504d79987c01b8
shahidb89 <shahid_b89@hotmail.com>
2021-09-15 07:10:01 +0100

    Multiple Location Version

--- Final.py
+++ Final.py
@@ -49,15 +49,15 @@ def get_BLAT_seqs(input_1, input_2, inserted_seq):
             f_list.append(ls)
 
 
-    seq = ''
+    ref_seq = ''
 
-    with open('LTR.fa', "r") as LTR:
-            for line in LTR:
+    with open('{}'.format(inserted_seq), "r") as i_file:
+            for line in i_file:
                 if not line.startswith('>'):
-                    seq += line.strip()
+                    ref_seq += line.strip()
     c = 1               
-    with open("soft_reads.fasta", 'a') as s_file:
-        s_file.write(">ref\n" + seq + "\n")
+    with open("soft_reads.fasta", 'w') as s_file:
+        s_file.write(">ref\n" + ref_seq + "\n")
         for ls in f_list:
             s_file.write(">seq" + str(c) + "\n" + ls[-4] + "\n")
             c += 1
@@ -68,7 +68,7 @@ def get_BLAT_seqs(input_1, input_2, inserted_seq):
     with open('aln.fasta', 'r') as f:
         seq_str = f.read()
 
-    seq_list = seq_str.split('>')
+        seq_list = seq_str.split('>')
 
     final_seq_list = []
 
@@ -79,59 +79,120 @@ def get_BLAT_seqs(input_1, input_2, inserted_seq):
                 temp +=l
         final_seq_list.append(temp)
 
-
     del final_seq_list[0]
-    final_seq_list
+    final_seq_list[0]
 
-    final_seq_list_sorted = sorted(final_seq_list)
+    start_index_5 = 0
+    start_index_3 = 0
 
-    final_seqs_cln = []
-    temp = ''
-
-    for l in final_seq_list[0]:
-        if l != '-':
-            temp += l
-    final_seqs_cln.append(temp)
-    temp = ''
-
-    for l in final_seq_list_sorted[0]:
-        if l != '-':
-            temp += l
-    final_seqs_cln.append(temp)
-    temp = ''
-    for l in final_seq_list_sorted[-1]:
-        if l != '-':
-            temp += l
-    final_seqs_cln.append(temp)
-
-
-
-    # Capturing the length of overhanging sequences
-    ltr_5 = 0
-    ltr_3 = 0
-    a = -1
-    for n in final_seq_list[0]:
-        if n in '-':
-            ltr_5 += 1
+    for i in final_seq_list[0]:
+        if i in "-":
+            start_index_5 +=1
         else:
             break
+
     c = -1       
     for n in range (len(final_seq_list[0])):
         if final_seq_list[0][c] in '-':
-            ltr_3 += 1
+            start_index_3 += 1
             c -= 1
         else:
             break
+
+    final_seq_list.sort()
+    for i, seq in enumerate(final_seq_list):
+        if ref_seq in seq:
+            ref_seq_index = i
+
+    list_of_5_seqs = final_seq_list[ref_seq_index+1:]
+    list_of_3_seqs = final_seq_list[:ref_seq_index]
+
+
+    list_of_5_seqs_cln = []
+    list_of_3_seqs_cln = []
+
+    for i in list_of_5_seqs:
+        i = i[:start_index_5].replace("-","")
+        list_of_5_seqs_cln.append(i)
+
+    def sort_5_seqs(seq):
+        return seq[::-1]
+
+    list_of_5_seqs_cln = list(set(list_of_5_seqs_cln))
+    list_of_5_seqs_cln_sorted = sorted(list_of_5_seqs_cln, key=sort_5_seqs)
+
+    list_of_5_seqs_cln_sorted.append("")
+
+    for i in list_of_3_seqs:
+        i = i[-start_index_3:].replace("-",'')
+        list_of_3_seqs_cln.append(i)
+
+    list_of_3_seqs_cln = list(set(list_of_3_seqs_cln))
+    list_of_3_seqs_cln_sorted = sorted(list_of_3_seqs_cln) 
+
+    list_of_3_seqs_cln_sorted.append("")
+
+    results_3_end = []
+
+    a = 0
+    b = 1
+    c = 2
+
+
+    while a < (len(list_of_3_seqs_cln_sorted)-2):
+        seq_1 = list_of_3_seqs_cln_sorted[a]
+        seq_2 = list_of_3_seqs_cln_sorted[b]
+        seq_3 = list_of_3_seqs_cln_sorted[c]
+        if seq_1 in seq_2 and seq_2 not in seq_1 and len(seq_1)<len(seq_2)>len(seq_3)and seq_2.startswith(seq_1):
+            results_3_end.append(seq_2)
+            a+=1
+            b+=1
+            c+=1
+        else:
+            pass
+
+            a+=1
+            b+=1
+            c+=1
+            
+    results_5_end = []
+    a = 0
+    b = 1
+    c = 2
+    while a < (len(list_of_5_seqs_cln_sorted)-2):
+        seq_1 = list_of_5_seqs_cln_sorted[a]
+        seq_2 = list_of_5_seqs_cln_sorted[b]
+        seq_3 = list_of_5_seqs_cln_sorted[c]
+        if seq_1 == seq_2:
+            pass
+        if (seq_1 in seq_2) and (seq_2 not in seq_1) and len(seq_1)<len(seq_2)>len(seq_3) and seq_2.endswith(seq_1):
+            results_5_end.append(seq_2)
+            a+=1
+            b+=1
+            c+=1
+        else:
+            pass
+
+            a+=1
+            b+=1
+            c+=1
     # Creating BLAT serach ready final list of sequences
 
     BLAT_ready_list = []
-
-    BLAT_ready_list.append(final_seqs_cln[2][:ltr_5])
-    BLAT_ready_list.append(final_seqs_cln[1][(len(final_seqs_cln[1])-ltr_3):])
+    BLAT_ready_list.append(results_5_end)
+    BLAT_ready_list.append(results_3_end)
+    
+    file_str = ">5' overhanging seqs\n "
+    for element in results_5_end:
+        file_str += '>' + element + '\n'
+        
+    file_str += "\n>3' overhanging seqs\n "
+    for element in results_3_end:
+        file_str += '>' + element + '\n'
     
     
-    with open("BLAT_ready_seqs.txt", 'a') as s_file:
-         s_file.write(">5'seq " +  BLAT_ready_list[0] + "\n" + ">3' seq " + BLAT_ready_list[1])
+    with open("BLAT_ready_seqs.txt", 'w') as s_file:
+         s_file.write(file_str)
          
     
     os.remove('aln.fasta') 
